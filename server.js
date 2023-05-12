04d6b7ba840c09567e2c824cb0b11de2c151ff33
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-05-12 13:14:35 +0100

    added env openshift

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const employeeRouter = require('./routers/employeeRouter.js');
 
+const mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || 'localhost';
+
 mongoose
-  .connect('mongodb://localhost/companydb')
+  .connect(`mongodb://${mongoURL}/companydb`)
   .then(() => {
     console.log('connected to db');
   })
@@ -18,10 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/employee', employeeRouter);
 
-const port = 5000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+const ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`serve at http://${ip}:${port}`);
 });
 
 module.exports = app;
