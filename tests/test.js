9ebe3817dd7222eed3197cf5f7da30c4d489c9ce
MsTYsN <yassinemestaoui98@gmail.com>
2023-05-12 11:14:37 +0100

    added chai testing

---
+++ tests/test.js
@@ -0,0 +1,31 @@
+var server   = require('../server'),
+    chai     = require('chai'),
+    chaiHTTP = require('chai-http'),
+    should   = chai.should();
+
+chai.use(chaiHTTP);
+
+reqServer = process.env.HTTP_TEST_SERVER || server
+
+describe('Basic routes tests', function() {
+
+    it('GET to /api/employee should return 200', function(done){
+        chai.request(reqServer)
+        .get('/api/employee')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            done();
+        })
+
+    })
+
+    it('POST to /api/employee should return 200', function(done){
+        chai.request(reqServer)
+        .get('/api/employee')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            done();
+        })
+
+    })
+})
